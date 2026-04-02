1d6798a5e59d2cd22b3750152bda45fac4de3fc9
meats88 <pcx883mi@gmail.com>
2026-04-01 19:08:48 -0700

    donenoen

--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 int main()
 {
 	int n1, n2;
-	int N, sum;
-
+	int N, nextval;
+cout << " enter two numbers" << endl;
 	cin >> n1 >> n2;
+	cout << "enter number of sequences" << endl;
 	cin >> N;
+cout << n1 << "\t" << n2 << "\t";
 
 	// TODO
-	// TODO
+	for ( int i =0; i < N - 2; i++){
+		nextval = n1 + n2;
+		cout << nextval << "\t";
+		n1 = n2;
+		n2 = nextval;
+
+	}
+	cout << endl;
+	return 0;
 }
